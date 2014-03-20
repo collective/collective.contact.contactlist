97cc8be735d6517ed71c6465b20052e8fef0e6b3
Thomas Desvenain <thomas.desvenain@gmail.com>
2014-03-20 10:55:47 +0100

    export link on contact list content

--- src/collective/contact/contactlist/browser/excelexport.py
+++ src/collective/contact/contactlist/browser/excelexport.py
@@ -1,8 +1,12 @@
 from zope.component import adapts
+from zope.i18n import translate
+
+from plone.app.layout.viewlets.common import ViewletBase
 
 from collective.excelexport.datasources.base import BaseContentsDataSource
 from collective.contact.contactlist.interfaces import IContactList,\
     ICollectiveContactContactlistLayer
+from collective.contact.contactlist import _
 
 
 class ContactListDataSource(BaseContentsDataSource):
@@ -13,3 +17,18 @@ class ContactListDataSource(BaseContentsDataSource):
 
     def get_objects(self):
         return [r.to_object for r in self.context.contacts if r.to_object]
+
+
+class ExportLinkViewlet(ViewletBase):
+
+    def render(self):
+        url = "%s/@@collective.excelexport" % self.context.absolute_url()
+        title = translate(_(u"Excel export list of contacts"),
+                          context=self.request)
+        portal_url = self.site_url
+        return """<a title="%(title)s" href="%(url)s">
+                     <img src="%(portal_url)s/xls.png" />
+                     %(title)s
+                  </a>""" % {'url': url,
+                             'title': title,
+                             'portal_url': portal_url}
